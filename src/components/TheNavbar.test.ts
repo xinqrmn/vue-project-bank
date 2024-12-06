5e3775579187b62d40d12664b0b42c875c6d76ca
Roman Galanov <mongo4558@gmail.com>
2024-12-06 16:44:43 +0400

    Added unit test compiler

---
+++ src/components/TheNavbar.test.ts
@@ -0,0 +1,8 @@
+// import {render} from '@testing-library/vue'
+// import TheNavbar from './TheNavbar.vue'
+//
+// test('renders TheNavbar', () => {
+//
+//   const {debug} = render(TheNavbar)
+//   debug()
+// })
